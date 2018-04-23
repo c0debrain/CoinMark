90b291f08c0ab47ac5a09fe5425a905e86c73eb1
blopez6 <blopez6@uncc.edu>
2018-04-23 18:31:47 -0400

    Still working on the chart

--- client/api/index.js
+++ client/api/index.js
@@ -4,18 +4,10 @@ import get from 'lodash.get';
 import { LOCAL_JSON_DATA_DIR } from '../constants';
 
 function getPriceHistoryUrl(cryptocurrency, currency, durationType) {
-  if (process.env.NODE_ENV !== 'production') {
-    return `${LOCAL_JSON_DATA_DIR}/${cryptocurrency}-${currency}_${durationType}.json`;
-  }
-
   return `https://www.coinbase.com/api/v2/prices/${cryptocurrency}-${currency}/historic?period=${durationType}`;
 }
 
 function getSpotPriceUrl(currency) {
-  if (process.env.NODE_ENV !== 'production') {
-    return `${LOCAL_JSON_DATA_DIR}/${currency}_spot.json`;
-  }
-
   return `https://api.coinbase.com/v2/prices/${currency}/spot?`;
 }
 
@@ -25,7 +17,7 @@ function fetchPriceHistory(cryptocurrency, currency, durationType) {
   return new Promise((resolve, reject) => {
     axios
       .get(url)
-      .then((response) => {
+      .then(response => {
         const priceHistory = get(response, ['data', 'data', 'prices'], []);
         const formattedPriceHistory = priceHistory
           .sort((a, b) => new Date(a.time) - new Date(b.time))
@@ -42,7 +34,7 @@ function fetchSpotPrices(currency) {
   return new Promise((resolve, reject) => {
     axios
       .get(url)
-      .then((response) => {
+      .then(response => {
         const spotPrices = get(response, ['data', 'data'], []);
         const formattedSpotPrices = spotPrices
           .filter(e => ['BTC', 'BCH', 'ETH', 'LTC'].indexOf(e.base) >= 0)

f2895a00563b19e30c2d42bf7463f98503f9d3f8
blopez6 <blopez6@uncc.edu>
2018-04-23 20:28:52 -0400

    Added header

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Chart from '../containers/Chart';
+import Header from './Header/';
 import '../reset.css';
 import '../main.css';
 
 export default () => {
   return (
     <div>
+      <Header />
       <Chart />
     </div>
   );
---
+++ client/components/Header/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="nav navbar-default">
+        <div className="navbar-header">
+          <a className="navbar-brand">CoinMark</a>
+        </div>
+        <ul className="nav navbar-nav navbar-right">
+          <li className="userIcon">
+            <a className="glyphicon glyphicon-user" />
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Header;

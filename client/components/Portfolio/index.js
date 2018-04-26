8d35d791f68410a0b5824453f7f158b9f9fdb707
zgadoury <zgadoury@uncc.edu>
2018-04-25 20:35:06 -0400

    Hide show functionality working

--- client/components/Portfolio/index.js
+++ client/components/Portfolio/index.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react';
 import RecentPurchases from './components/RecentPurchases';
 import Wallet from './components/Wallet';
 class Portfolio extends Component {
+
+  //Constructor inherits main class functionality. Sets states of both headers to showing true.
+  constructor(){
+    super()
+    this.state={
+      showMeOne:true
+    }
+    this.state={
+      showMeTwo:true
+    }
+  }
+  //method for showing RecentPurchases
+  showHideTwo(){
+  this.setState({
+    showMeTwo:!this.state.showMeTwo
+  })
+}
+  //method for showing Wallet
+  showHideOne(){
+    this.setState({
+      showMeOne:!this.state.showMeOne
+    })
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-md-5">
-          <span class="pull-right">
+          <span class="pull-left">
+          <h1 className="headers" onClick={()=>this.showHideTwo()}> Recent Purchases</h1>
+            {
+              this.state.showMeTwo?
             <RecentPurchases />
+            :null
+            }
           </span>
         </div>
         <div className="col-md-5">
           <span class="pull-right">
-            <Wallet />
+          <h1 className="headers" onClick={()=>this.showHideOne()}>Wallet</h1>
+      {
+        this.state.showMeOne?
+        <Wallet />
+        :null
+      }
           </span>
         </div>
       </div>

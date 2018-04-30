8f1db316b5f5f9bccdd43d28c336172dbdbec023
blopez6 <blopez6@uncc.edu>
2018-04-30 14:20:44 -0400

    updated collections

--- imports/collections/buys.js
+++ imports/collections/buys.js
@@ -12,56 +12,6 @@ Meteor.methods({
 
   'buys.remove': function(buy) {
     return Buys.remove(buy);
-  },
-
-  'buys.update': function(buy, coinAmount) {
-    return Bins.update(buy._id, { $set: { coinAmount } });
-  },
-
-  Bitcoinupdate(coinAmount) {
-    return Buys.update({
-      coinAmount: coinAmount,
-      coinType: 'Bitcoin',
-      ownerId: this.userId
-    });
-  },
-
-  /*incrementCoin: function(coinType) {
-    check(coinType, Meteor.Collection.ObjectID);
-
-    Buys.update(
-      {
-        _id: coinType
-      },
-      {
-        $inc: {
-          coinAmount: 1
-        }
-      },
-      function(error, affectedDocs) {
-        if (error) {
-          throw new Meteor.Error(500, error.message);
-        } else {
-          return 'Update Successful';
-        }
-      }
-    );
-  },*/
-
-  incrementCoin: function(coinType) {
-    Buys.update({ ownerId: this.userId }, { $inc: { coinAmount: 1 } });
-  },
-
-  'buys.update': function(coinAmount) {
-    Buys.update(buy, { $inc: { coinAmount: coinAmount } });
-  },
-
-  'buys.purchase': function(buy, coinAmount) {
-    return Buys.update(buy._id, { $inc: { coinAmount } });
-  },
-
-  'buys.wallet': function(buy, coinAmount) {
-    return Buys.update(buy.coinType, { $inc: { coinAmount } });
   }
 });
 

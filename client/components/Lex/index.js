26fd5bc408b86357c4bd8fa9f8d872e80c841d0f
crbardon <crb0322@gmail.com>
2018-05-02 16:48:39 -0400

    Updates to lexbot

--- client/components/Lex/index.js
+++ client/components/Lex/index.js
@@ -44,9 +44,102 @@ class Lex extends Component {
       );
       conversation.advanceConversation();
     }
+        var particleAlphabet = {
+        Particle: function(x, y) {
+            this.x = x;
+            this.y = y;
+            this.radius = 3.5;
+            this.draw = function(ctx) {
+                ctx.save();
+                ctx.translate(this.x, this.y);
+                ctx.fillStyle = '#4b0082';
+                ctx.fillRect(0, 0, this.radius, this.radius);
+                ctx.restore();
+            };
+        },
+        init: function() {
+            console.log('INIT');
+            particleAlphabet.canvas = document.getElementById('canvasParticles');
+            particleAlphabet.ctx = particleAlphabet.canvas.getContext('2d');
+            particleAlphabet.W = 800;
+            particleAlphabet.H = 165;
+            particleAlphabet.particlePositions = [];
+            particleAlphabet.particles = [];
+            particleAlphabet.tmpCanvas = document.createElement('canvas');
+            particleAlphabet.tmpCtx = particleAlphabet.tmpCanvas.getContext('2d');
+
+            particleAlphabet.canvas.width = particleAlphabet.W;
+            particleAlphabet.canvas.height = particleAlphabet.H;
+
+            setInterval(function(){
+                particleAlphabet.changeLetter();
+                particleAlphabet.getPixels(particleAlphabet.tmpCanvas, particleAlphabet.tmpCtx);
+            }, 1200);
+
+            particleAlphabet.makeParticles(1000);
+            particleAlphabet.animate();
+        }, 
+        currentPos: 0,
+        changeLetter: function() {
+            var letters = 'GET A COIN, BUY A COIN, SELL A COIN',
+                letters = letters.split(', ');
+            particleAlphabet.time = letters[particleAlphabet.currentPos];
+            particleAlphabet.currentPos++;
+            if (particleAlphabet.currentPos >= letters.length) {
+                particleAlphabet.currentPos = 0;
+            }
+        },
+        makeParticles: function(num) {
+            for (var i = 0; i <= num; i++) {
+                particleAlphabet.particles.push(new particleAlphabet.Particle(particleAlphabet.W / 2 + Math.random() * 400 - 200, particleAlphabet.H / 2 + Math.random() * 400 -200));
+            }
+        },
+        getPixels: function(canvas, ctx) {
+            var keyword = particleAlphabet.time,
+                gridX = 6,
+                gridY = 6;
+            canvas.width = 800;
+            canvas.height = 165;
+            ctx.fillStyle = 'red';
+            ctx.font = 'bold 115px Arial';
+            ctx.fillText(keyword, canvas.width / 2 - ctx.measureText(keyword).width / 2, canvas.height / 2);
+            var idata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            var buffer32 = new Uint32Array(idata.data.buffer);
+            if (particleAlphabet.particlePositions.length > 0) particleAlphabet.particlePositions = [];
+            for (var y = 0; y < canvas.height; y += gridY) {
+                for (var x = 0; x < canvas.width; x += gridX) {
+                    if (buffer32[y * canvas.width + x]) {
+                        particleAlphabet.particlePositions.push({x: x, y: y});
+                    }
+                }
+            }
+        },
+        animateParticles: function() {
+            var p, pPos;
+            for (var i = 0, num = particleAlphabet.particles.length; i < num; i++) {
+                p = particleAlphabet.particles[i];
+                pPos = particleAlphabet.particlePositions[i];
+                if (particleAlphabet.particles.indexOf(p) === particleAlphabet.particlePositions.indexOf(pPos)) {
+                p.x += (pPos.x - p.x) * .3;
+                p.y += (pPos.y - p.y) * .3;
+                p.draw(particleAlphabet.ctx);
+            }
+            }
+        },
+        animate: function() {
+            requestAnimationFrame(particleAlphabet.animate);
+            particleAlphabet.ctx.fillStyle = 'rgba(255, 255, 255, .8)';
+            console.log(particleAlphabet.ctx.fillStyle);
+            particleAlphabet.ctx.fillRect(0, 0, particleAlphabet.W, particleAlphabet.H);
+            particleAlphabet.animateParticles();
+        }
+    };
+    
+    window.onload = particleAlphabet.init;
 
     return (
       <div id="CoinBot">
+        <canvas id="canvasParticles"></canvas>
         <div className="audio-control">
           <p>
             <h3>Click on microphone to active!</h3>
@@ -55,7 +148,7 @@ class Lex extends Component {
           <p id="audio-control" className="white-circle">
             <img
               onClick={awscall}
-              src="https://static1.squarespace.com/static/575e8c3c86db43d483834dc6/t/599c143e8fd4d24d1b11838d/1503401031161/The+Voice+Agency+Microphone.png"
+              src="http://chittagongit.com//images/microphone-icon-png/microphone-icon-png-11.jpg"
             />
           </p>
           <p>

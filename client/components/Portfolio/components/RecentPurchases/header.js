da99b700b885fbd89042e6077e0440847a3a82cf
crbardon <crb0322@gmail.com>
2018-05-02 20:43:16 -0400

    header signifiers updated

--- client/components/Portfolio/components/RecentPurchases/header.js
+++ client/components/Portfolio/components/RecentPurchases/header.js
@@ -4,9 +4,16 @@ import './index.css';
 
 class RecentHeader extends Component {
   render() {
+    function highlight(e){
+      document.getElementById('purchasesheader').style.backgroundColor = '#4b00b0';
+    }
+    
+    function unhighlight(e){
+      document.getElementById('purchasesheader').style.backgroundColor = '#4b0082';
+    }
     return (
       <div>
-        <h1 className="headersport" tabindex='8' role='button'>Recent Purchases</h1>
+        <h1 id='purchasesheader' onMouseEnter={highlight} onMouseOut={unhighlight} className="headersport" tabIndex='8' role='button'>Recent Purchases</h1>
       </div>
     );
   }
--- client/components/Portfolio/components/Wallet/header.js
+++ client/components/Portfolio/components/Wallet/header.js
@@ -4,9 +4,16 @@ import './index.css';
 
 class WalletHeader extends Component {
   render() {
+    function highlight(e){
+      document.getElementById('walletheader').style.backgroundColor = '#4b00b0';
+    }
+    
+    function unhighlight(e){
+      document.getElementById('walletheader').style.backgroundColor = '#4b0082';
+    }
     return (
       <div>
-        <h1 className="headersport" tabindex='9' role='button'>Wallet</h1>
+        <h1 id='walletheader' onMouseEnter={highlight} onMouseOut={unhighlight} className="headersport" tabIndex='9' role='button'>Wallet</h1>
       </div>
     );
   }
